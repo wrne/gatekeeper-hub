c1e8988e723451200a4a3af975d057ea7e686954
Wanderley Neto <wanderleyneto@Mac-de-Wanderley.local>
2025-01-28 16:43:36 -0300

    Implantação do modo 'development'

--- js/summer/src/server.js
+++ js/summer/src/server.js
@@ -1,13 +1,17 @@
 import app from "./express.js";
 import http from "http";
 
+// Verifica se possui o argumento de modo na chamada do app
+const developmentMode = process.argv.includes("--dev-mode");
 const port = process.env.port || 3000;
+
 
 // Define variável global para definir configurações da fila de mensagens
 process.summer = {
 	exchanges: {
 		QUEUE_ORDERS : 'orders_teste'
-	}
+	},
+	MODE: (developmentMode ? 'development' : 'production')
 }
 
 http.createServer(app).listen(port, function () {
